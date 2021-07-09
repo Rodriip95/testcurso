399865ead9d8e859b7092035c377b5dfe18afe2c
Federico Lencina <federiconahuellencina@gmail.com>
2021-07-09 00:02:06 -0300

    Test finalizados

--- index.js
+++ index.js
@@ -1,13 +1,99 @@
-exports.Sumar = function(a, b){
-    return a + b
+function Sumar (a, b){
+    //Devuelve la suma entre a y b
+    return null
 }
 
-exports.Restar = function(a, b){
-    return a - b
+function Restar (a, b){
+    //Devuelve la resta entre a y b
+    return null
 }
 
-exports.Restarx2 = function(a, b){
-    return (a - b)*2
+function esMayor (a){
+    //Devuelve True si la persona es mayor de edad y false en caso contrario.
+    return null
 }
+
+function esHashtag (palabra){
+    //Devuelve True si el primer caracter es un # y false en caso contrario.
+    return null
+}
+
+function esPar(a){
+    //Retorna True si el numero es par y false en caso contrario. Un numero es par si al dividirlo por 2 da resto 0. (Nota usar el % que devuelve el resto de una division si es 0 es par, sino es impar).
+    return null
+}
+
+function mismaLetraInicial (palabra1, palabra2){
+    //Devuelve True si ambas palabras tienen la misma letra inicial,false en caso contrario.(Nota: usar la funcion .charAt() de strings).
+    return null
+}
+
+function cantidadDeA(palabra){
+    //Retorna la cantidad de letras a que contiene la palabra. Si no contiene a, devuelve 0.
+    return null
+}
+
+function Potencia (a, b){
+    //Devuelve como resultado la potencia de a elevado por b. Si b es 0 retornar 1. y si a es 0 retornar 0.
+    return null
+}
+
+function cantidadDeVocales(palabra){
+    //Retorna la cantidad de vocales que contiene la palabra. Si no contiene vocal, devuelve 0.
+    return null
+}
+
+function mismaCantidadCaracteres (palabra1, palabra2){
+    //Devuelve True si la palabra 2 contiene la misma cantidad de caracteres que la palabra 1,false en caso contrario.(Nota: usar la funcion .lenght de strings).
+    return null
+}
+
+function estaAprobado(nota1,nota2,nota3){
+    //Retorna True si el promedio entre las 3 notas es mayor o igual a 7, sino retorna false.
+    return null
+}
+
+function Resultado(nota1,nota2,nota3){
+    //Retorna "Aprobado" si el promedio entre las 3 notas es mayor o igual a 7, "Regular" si es mayor o igual a 4 y menor que 7 y "Desaprobado" si es menor que 4.
+    return null
+}
+
+function palabraEspecial(palabra){
+    //Debe retornar la palabra con un * adelante y otro atra, ejemplo: *Hola*.Si tiene un asterisco tanto adelante y/o atras, no agregarlo.
+    return null
+}
+
+function Concatenar(nombre,apellido){
+    //Debe devolver "Apellido, Nombre", ej: "Perez, Juan".
+    return null
+
+}
+
+function cantidadDeParesHasta(numero){
+    //Debe retornar la cantidad de pares desde el 0 hasta el numero inclusive pasado por parametro, ej: cantidadDeParesHasta(10) retorna 5.
+    //Consejo, se puede utilizar la funcion esPar.
+    return null
+}
+
+
+
+module.exports = {
+    Sumar,
+    Restar,
+    esMayor,
+    Potencia,
+    esHashtag,
+    esPar,
+    Resultado,
+    mismaCantidadCaracteres,
+    mismaLetraInicial,
+    cantidadDeA,
+    cantidadDeVocales,
+    estaAprobado,
+    Concatenar,
+    palabraEspecial,
+    cantidadDeParesHasta
+    
+  }
 
 
--- test/test.js
+++ test/test.js
@@ -1,18 +1,280 @@
 const chai = require("chai")
 const expect = chai.expect
 
-const funciones_test = require("../index")
+const {
+  Sumar,
+  Restar,
+  esMayor,
+  Potencia,
+  esHashtag,
+  esPar,
+  Resultado,
+  mismaCantidadCaracteres,
+  mismaLetraInicial,
+  cantidadDeA,
+  cantidadDeVocales,
+  estaAprobado,
+  Concatenar,
+  palabraEspecial,
+  cantidadDeParesHasta
+} = require("../index")
 
 describe('Test de funciones: ', function() {
     describe('Check function Sumar', function(){
       it('TestCase Suma basica: ', function() {
-         result = funciones_test.Sumar(2,5)
+         result = Sumar(2,5)
           expect(result).to.equal(7);
       })
+      
+      it('Suma -2 + -2: ', function() {
+        result = Sumar(-2,-2)
+         expect(result).to.equal(-4);
+      })
 
+      it('Suma 3 + 8: ', function() {
+        result = Sumar(3,8)
+         expect(result).to.equal(11);
+      }) 
+    })
+
+    describe('Check function Restar', function(){
       it('TestCase Resta basica: ', function() {
-        result = funciones_test.Restar(12,5)
-         expect(result).to.equal(7);
-     })
+         result = Restar(5,2)
+          expect(result).to.equal(3);
+      })
+      
+      it('Resta -2 -(-2): ', function() {
+        result = Restar(-2,-2)
+         expect(result).to.equal(0);
+      })
+
+      it('Resta 3 - 8: ', function() {
+        result = Restar(3,8)
+         expect(result).to.equal(-5);
+      })
+    })
+
+    describe('Check function esMayor', function(){
+      it('TestCase esMayor: ', function() {
+         result = esMaypor(18)
+          expect(result).to.equal(true);
+      })
+      
+      it('esMayor(25): ', function() {
+        result = esMayor(25)
+         expect(result).to.equal(true);
+      })
+
+      it('esMayor(10): ', function() {
+        result = esMayor(10)
+         expect(result).to.equal(false);
+      })
+    })
+
+    describe('Check function esHashtag', function(){
+      it('TestCase esHashtag: ', function() {
+         result = esHashtag("#JavaScript")
+          expect(result).to.equal(true);
+      })
+      
+      it('esHashtag("JavaScript"): ', function() {
+        result = esHashtag("JavaScript")
+         expect(result).to.equal(false);
+      })
+
+      it('esHashtag(#): ', function() {
+        result = esHash("#")
+         expect(result).to.equal(true);
+      })
+    })
+
+    describe('Check function esPar', function(){
+      it('TestCase esPar: ', function() {
+         result = esPar(2)
+          expect(result).to.equal(true);
+      })
+      
+      it('esPar("3"): ', function() {
+        result = esPar(3)
+         expect(result).to.equal(false);
+      })
+
+      it('esPar("4"): ', function() {
+        result = esPar(4)
+         expect(result).to.equal(true);
+      })
+    })
+
+    describe('Check function mismaLetraInicial', function(){
+      it('TestCase mismaLetraInicial: ', function() {
+         result = mismaLetraInicial("hola","helado")
+          expect(result).to.equal(true);
+      })
+      
+      it('mismaLetraInicial("atras","hatras"): ', function() {
+        result = mismaLetraInicial("atras","hatras")
+         expect(result).to.equal(false);
+      })
+
+      it('mismaLetraInicial("",""): ', function() {
+        result = mismaLetraInicial("","")
+         expect(result).to.equal(true);
+      })
+    })
+
+    describe('Check function cantidadDeA', function(){
+      it('TestCase cantidadDeA: ', function() {
+         result = cantidadDeA("abracadabra")
+          expect(result).to.equal(5);
+      })
+      
+      it('cantidadDeA("hello"): ', function() {
+        result = cantidadDeA("hello")
+         expect(result).to.equal(0);
+      })
+
+      it('cantidadDeA(""): ', function() {
+        result = cantidadDeA("")
+         expect(result).to.equal(0);
+      })
+    })
+
+    describe('Check function potencia', function(){
+      it('TestCase potencia: ', function() {
+         result = Potencia(2,3)
+          expect(result).to.equal(8);
+      })
+      
+      it('Potencia(2,3): ', function() {
+        result = Potencia(0,3)
+         expect(result).to.equal(0);
+      })
+
+      it('Potencia(100,0): ', function() {
+        result = Potencia(100,0)
+         expect(result).to.equal(1);
+      })
+    })
+
+    describe('Check function cantidadDeVocales', function(){
+      it('TestCase cantidadDeVocales: ', function() {
+         result = cantidadDeVocales("aeiou")
+          expect(result).to.equal(5);
+      })
+      
+      it('cantidadDeVocales("maxiconsumo"): ', function() {
+        result = cantidadDeVocales("maxiconsumo")
+         expect(result).to.equal(5);
+      })
+
+      it('cantidadDeVocales("HTML"): ', function() {
+        result = cantidadDeVocales("HTML")
+         expect(result).to.equal(0);
+      })
+    })
+
+    describe('Check function mismaCantidadCaracteres', function(){
+      it('TestCase mismaCantidadCaracteres: ', function() {
+         result = mismaCantidadCaracteres("aeiou","aeiou")
+          expect(result).to.equal(true);
+      })
+      
+      it('mismaCantidadCaracteres("","aeiou"): ', function() {
+        result = mismaCantidadCaracteres("","aeiou")
+         expect(result).to.equal(false);
+      })
+
+      it('mismaCantidadCaracteres("basquetbal","atletismos"): ', function() {
+        result = mismaCantidadCaracteres("basquetbal","atletismos")
+         expect(result).to.equal(true);
+      })
+    })
+
+    describe('Check function estaAprobado', function(){
+      it('TestCase estaAprobado: ', function() {
+         result = estaAprobado(8,9,8)
+          expect(result).to.equal(true);
+      })
+      
+      it('estaAprobado(5,4,6): ', function() {
+        result = estaAprobado(5,4,6)
+         expect(result).to.equal(false);
+      })
+
+      it('estaAprobado(7,6,0): ', function() {
+        result = estaAprobado(7,6,0)
+         expect(result).to.equal(false);
+      })
+    })
+
+    describe('Check function Resultado', function(){
+      it('TestCase Resultado: ', function() {
+         result = Resultado(8,9,8)
+          expect(result).to.equal("Aprobado");
+      })
+      
+      it('Resultado(5,4,6): ', function() {
+        result = Resultado(5,4,6)
+         expect(result).to.equal("Regular");
+      })
+
+      it('Resultado(1,2,3): ', function() {
+        result = Resultado(1,2,3)
+         expect(result).to.equal("Desaprobado");
+      })
+    })
+
+    describe('Check function palabraEspecial', function(){
+      it('TestCase palabraEspecial: ', function() {
+         result = palabraEspecial("hola")
+          expect(result).to.equal("*hola*");
+      })
+      
+      it('palabraEspecial("JS"): ', function() {
+        result = palabraEspecial("JS")
+         expect(result).to.equal("*JS*");
+      })
+
+      it('palabraEspecial("*hola*"): ', function() {
+        result = palabraEspecial("")
+         expect(result).to.equal("*hola*");
+      })
+    })
+
+    describe('Check function Concatenar', function(){
+      it('TestCase Concatenar: ', function() {
+         result = Concatenar("Rodrigo", "Penela")
+          expect(result).to.equal("Penela, Rodrigo");
+      })
+      
+      it('Concatenar("Juan", "Perez"): ', function() {
+        result = Concatenar("Juan", "Perez")
+         expect(result).to.equal("Perez, Juan");
+      })
+
+      it('Concatenar("Esteban", "Quito"): ', function() {
+        result = Concatenar("Esteban", "Quito")
+         expect(result).to.equal("Quito, Esteban");
+      })
+    })
+
+    describe('Check function cantidadParesHasta', function(){
+      it('TestCase cantidadParesHasta: ', function() {
+         result = cantidadParesHasta(10)
+          expect(result).to.equal(5);
+      })
+      
+      it('cantidadParesHasta(20): ', function() {
+        result = Concatenar(20)
+         expect(result).to.equal(10);
+      })
+
+      it('cantidadParesHasta(1): ', function() {
+        result = cantidadParesHasta(1)
+         expect(result).to.equal(0);
+      })
     })
   })
+
+
+
